b23d21db4c8119d6f7e76f1b054a173ea134dd6d
Tom MacWright <macwright@gmail.com>
2010-11-10 19:30:59 -0500

    No-dependencies streaming reverse proxy cache

---
+++ tileox.js
@@ -0,0 +1,55 @@
+// A proxy for tilejive
+
+var sys = require('sys'), http = require('http'), url = require('url');
+var apiPort = process.ARGV[2] || 8000;
+var toPort = process.ARGV[3] || 8888;
+var toHost = '127.0.0.1';
+var cache = {};
+
+http.createServer(function(req, res) {
+    var parsedUrl = url.parse(req.url),
+        response = '';
+
+    var fragment = parsedUrl.pathname + (parsedUrl.search || '');
+
+    if (cache[fragment]) {
+        res.writeHead(200, cache[fragment].headers);
+        res.write(cache[fragment].body);
+        res.end();
+    }
+
+    var client = http.createClient(toPort, toHost);
+
+    var request = client.request('GET', fragment, {
+        Host: toHost,
+        Accept: '*/*',
+        'User-Agent' : 'Mozilla/5.0 (compatible; MSIE 6.0; Windows NT5.0)',
+        'Accept-Language' : 'en-us',
+        'Accept-Charset' : 'ISO-8859-1,utf-8;q=0.7,*;q=0.7'
+    });
+
+    request.addListener('response', function(response) {
+        var body = '';
+        var start = true;
+        response.setEncoding('utf8');
+        response.addListener('data', function(chunk) {
+            if (start) {
+                res.writeHead(200, response.headers);
+                start = false;
+            }
+            res.write(chunk);
+            body += chunk;
+        });
+        response.addListener('end', function() {
+            cache[fragment] = {
+                body: body,
+                headers: response.headers
+            };
+            return res.end();
+        });
+    });
+
+    request.end();
+}).listen(apiPort);
+
+sys.puts('Server running at http://127.0.0.1:' + apiPort);
