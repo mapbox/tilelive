9ae53a5de5aa5f3220a8ead900fcaa0b7a637454
Tom MacWright <macwright@gmail.com>
2010-11-12 15:49:34 -0500

    Adding mappool and format

---
+++ tl/format.js
@@ -0,0 +1,43 @@
+var Format = {
+    /**
+     * Select a format
+     */
+    select: function(format) {
+        for (i in Format) {
+            if (Format[i].hasOwnProperty('find')) {
+                if (Format[i].find) {
+                    return module.exports.Format[i].render;
+                }
+            }
+        }
+    },
+
+    /**
+     * Generate a PNG file and call callback
+     * @param Function callback the function to call when
+     *  data is rendered.
+     */
+    png: {
+        'render': function(tile, callback) {
+            // TODO: make explicitly mapnik_map call
+            tile.map.mapnik_map().render(
+                tile.bbox,
+                function(image) {
+                    callback(null, [
+                        image, {
+                            'Content-Type': 'image/png'
+                        }, 204]);
+                }
+            );
+        },
+        'find': /png/
+    },
+
+    jpg: function(callback) { },
+
+    grid: function(callback) { },
+
+    geojson: function(callback) { }
+}
+
+module.exports.Format = Format;
---
+++ tl/mappool.js
@@ -0,0 +1,35 @@
+var mapnik = require('mapnik');
+
+/**
+ * A pool of maps
+ */
+var MapPool = new function mappool() {
+    this.pool = [];
+    this.size = 10;
+    for (var i = 0; i < this.size; i++) {
+        this.pool.push([]);
+    }
+};
+
+/**
+ * get a map
+ */
+MapPool.get = function(mapfile) {
+    if (this.pool[mapfile]) {
+        // rotate queue
+        this.pool[mapfile].unshift(
+            this.pool[mapfile].pop());
+    } else {
+        // TODO: just use locking instead of this
+        this.pool[mapfile] = [];
+        for (var i = 0; i < this.size; i++) {
+            var m = new mapnik.Map(256, 256);
+            m.load(mapfile);
+            this.pool[mapfile].push(m);
+        }
+    }
+    console.log(this.pool[mapfile][0]);
+    return this.pool[mapfile][0];
+}
+
+module.exports.MapPool = MapPool;
