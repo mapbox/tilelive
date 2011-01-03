90e007a94de5cb0674ae677d4d5472d0ef43c68e
Will White <will@developmentseed.org>
2011-01-03 14:39:02 -0500

    Set proper file perms for newly created directories.

--- bootstrap.js
+++ bootstrap.js
@@ -10,14 +10,14 @@ function bootstrap() {
         fs.statSync(app.set('settings')('mapfile_dir'));
     } catch (Exception) {
         console.log('Creating mapfile dir %s', app.set('settings')('mapfile_dir'));
-        fs.mkdirSync(app.set('settings')('mapfile_dir'), 777);
+        fs.mkdirSync(app.set('settings')('mapfile_dir'), 0777);
     }
 
     try {
         fs.statSync(app.set('settings')('data_dir'));
     } catch (Exception) {
         console.log('Creating mapfile dir %s', app.set('settings')('data_dir'));
-        fs.mkdirSync(app.set('settings')('data_dir'), 777);
+        fs.mkdirSync(app.set('settings')('data_dir'), 0777);
     }
 }
 
