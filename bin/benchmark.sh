e978206f7479aeb57c25f5de3542f5eb73510cfe
Konstantin Käfer <github@kkaefer.com>
2011-08-01 14:02:47 -0400

    default concurrency is cpu cores * 25

--- bin/benchmark.sh
+++ bin/benchmark.sh
@@ -1,19 +1,22 @@
 #!/bin/sh
 
-for metatile in 1 2 3 4 6 8
+for metatile in 2 3 4
 do
-  for concurrency in 5 10 25 40 55 70 85 100 125 150 200
+  for zoom in 15 16 17
   do
-    rm devseed.mbtiles
-    bin/tilelive copy "mapnik://./devseed-hq.xml?metatile=$metatile" \
-        mbtiles://./devseed.mbtiles \
-        --bbox="-77.1292,38.8506,-76.9541,38.9644" \
-        --zoom=13-13 \
-        --copy-tiles \
-        --concurrency=$concurrency \
-        --benchmark
+    for concurrency in 25 50 75 100 150
+    do
+      rm devseed.mbtiles
+      bin/tilelive copy "mapnik://./devseed-hq.xml?metatile=$metatile" \
+          mbtiles://./devseed.mbtiles \
+          --bbox="-77.1292,38.8506,-76.9541,38.9644" \
+          --zoom=$zoom \
+          --copy-tiles \
+          --concurrency=$concurrency \
+          --benchmark
 
-    if [ $? -ne 0 ] ; then exit ; fi
+      if [ $? -ne 0 ] ; then exit ; fi
+    done
   done
 done
 
