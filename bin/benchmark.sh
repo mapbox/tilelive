ab82dae88968b4811ea85b58f4e507c84eaa4d93
Konstantin Käfer <github@kkaefer.com>
2011-08-01 15:15:23 -0400

    update script

--- bin/benchmark.sh
+++ bin/benchmark.sh
@@ -1,15 +1,18 @@
 #!/bin/sh
 
+STYLESHEET="./devseed-hq.xml"
+BBOX="-77.1292,38.8506,-76.9541,38.9644"
+
 for metatile in 2 3 4
 do
   for zoom in 15 16 17
   do
-    for concurrency in 25 50 75 100 150
+    for concurrency in 25 50 100 200
     do
-      rm devseed.mbtiles
-      bin/tilelive copy "mapnik://./devseed-hq.xml?metatile=$metatile" \
-          mbtiles://./devseed.mbtiles \
-          --bbox="-77.1292,38.8506,-76.9541,38.9644" \
+      rm out.mbtiles
+      bin/tilelive copy "mapnik://$STYLESHEET?metatile=$metatile" \
+          mbtiles://./out.mbtiles \
+          --bbox="$BBOX" \
           --zoom=$zoom \
           --copy-tiles \
           --concurrency=$concurrency \
