c3bc1757edc41e5d117272590e1626709bf738b1
mapbox <mapbox@mapbox.(none)>
2011-08-01 16:23:53 -0400

    also iterate over buffersize

--- bin/benchmark.sh
+++ bin/benchmark.sh
@@ -1,24 +1,27 @@
 #!/bin/sh
 
-STYLESHEET="./devseed-hq.xml"
+STYLESHEET="/home/mapbox/benchmarking/data/devseed-appliance.xml"
 BBOX="-77.1292,38.8506,-76.9541,38.9644"
 
-for metatile in 2 3 4
+for buffersize in 128 0 256
 do
-  for zoom in 15 16 17
+  for metatile in 2 3 4
   do
-    for concurrency in 25 50 100 200
+    for zoom in 15 16 17
     do
-      rm out.mbtiles
-      bin/tilelive copy "mapnik://$STYLESHEET?metatile=$metatile" \
-          mbtiles://./out.mbtiles \
-          --bbox="$BBOX" \
-          --zoom=$zoom \
-          --copy-tiles \
-          --concurrency=$concurrency \
-          --benchmark
+      for concurrency in 25 50 100 200
+      do
+        rm out.mbtiles
+        bin/tilelive copy "mapnik://$STYLESHEET?metatile=$metatile&bufferSize=$buffersize" \
+            mbtiles://./out.mbtiles \
+            --bbox="$BBOX" \
+            --zoom=$zoom \
+            --copy-tiles \
+            --concurrency=$concurrency \
+            --benchmark
 
-      if [ $? -ne 0 ] ; then exit ; fi
+        if [ $? -ne 0 ] ; then exit ; fi
+      done
     done
   done
 done
