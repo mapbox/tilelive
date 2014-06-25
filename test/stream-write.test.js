2c8069f2000300d3a8b8d81318d56f74dee1f47d
Young Hahn <young@mapbox.com>
2014-06-25 12:33:01 -0400

    Implement multiwrites.

--- test/stream-write.test.js
+++ test/stream-write.test.js
@@ -8,19 +8,22 @@ var Timedsource = require('./timedsource');
 test('write: slowput', function(t) {
     var fast = new Timedsource({time:5});
     var slow = new Timedsource({time:50});
-    var get = tilelive.createReadStream(slow, {type:'scanline'});
-    var put = tilelive.createWriteStream(fast);
+    var get = tilelive.createReadStream(fast, {type:'scanline'});
+    var put = tilelive.createWriteStream(slow);
     get.on('error', function(err) { t.ifError(err); });
     put.on('error', function(err) { t.ifError(err); });
     get.pipe(put);
     setTimeout(function() {
-        t.deepEqual(get.stats, { ops: 10, total: 85, skipped: 0, done: 0 });
-        t.deepEqual(put.stats, { ops: 1, total: 0, skipped: 0, done: 1 });
+        t.deepEqual(get.stats, { ops: 20, total: 85, skipped: 1, done: 10 });
+        t.deepEqual(put.stats, { ops: 10, total: 0, skipped: 0, done: 0 });
     }, 10);
     put.on('finish', function() {
         t.deepEqual(get.stats, { ops: 85, total: 85, skipped: 28, done: 85 });
-        t.deepEqual(put.stats, { ops: 58, total: 0, skipped: 0, done: 58 });
-        t.end();
+        // Multiwrites continue after 'finish' event.
+        setTimeout(function() {
+            t.deepEqual(put.stats, { ops: 58, total: 0, skipped: 0, done: 58 });
+            t.end();
+        }, 60);
     });
 });
 
