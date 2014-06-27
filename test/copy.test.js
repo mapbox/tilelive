6f1c8971ed59482fab608ec8e8532eafa65561cc
Young Hahn <young@mapbox.com>
2014-06-26 22:59:21 -0400

    Rename copy bin to tilelive-copy.

--- test/copy.test.js
+++ test/copy.test.js
@@ -8,7 +8,7 @@ var filepath = path.join(tmp, 'copy.mbtiles');
 try { fs.unlinkSync(filepath); } catch(e) {}
 
 test('copy usage', function(t) {
-    exec(__dirname + '/../bin/copy', function(err, stdout, stderr) {
+    exec(__dirname + '/../bin/tilelive-copy', function(err, stdout, stderr) {
         t.equal(1, err.code, 'exit 1');
         t.ok(/Usage:/.test(stdout), 'shows usage');
         t.end();
@@ -16,7 +16,7 @@ test('copy usage', function(t) {
 });
 
 test('copy copies', function(t) {
-    exec(__dirname + '/../bin/copy ' + __dirname + '/fixtures/plain_1.mbtiles ' + filepath, function(err, stdout, stderr) {
+    exec(__dirname + '/../bin/tilelive-copy ' + __dirname + '/fixtures/plain_1.mbtiles ' + filepath, function(err, stdout, stderr) {
         t.ifError(err, 'no errors');
         t.ok(stdout.indexOf('100.0000%') !== -1, 'pct complete');
         t.ok(stdout.indexOf('285/   285') !== -1, '285/285');
@@ -25,7 +25,7 @@ test('copy copies', function(t) {
 });
 
 test('copy min/max', function(t) {
-    exec(__dirname + '/../bin/copy --minzoom=1 --maxzoom=2 ' + __dirname + '/fixtures/plain_1.mbtiles ' + filepath, function(err, stdout, stderr) {
+    exec(__dirname + '/../bin/tilelive-copy --minzoom=1 --maxzoom=2 ' + __dirname + '/fixtures/plain_1.mbtiles ' + filepath, function(err, stdout, stderr) {
         t.ifError(err, 'no errors');
         t.ok(stdout.indexOf('100.0000%') !== -1, 'pct complete');
         t.ok(stdout.indexOf('20/    20') !== -1, '20/20');
@@ -34,7 +34,7 @@ test('copy min/max', function(t) {
 });
 
 test('copy bounds', function(t) {
-    exec(__dirname + '/../bin/copy --bounds=-180,-85,0,0 ' + __dirname + '/fixtures/plain_1.mbtiles ' + filepath, function(err, stdout, stderr) {
+    exec(__dirname + '/../bin/tilelive-copy --bounds=-180,-85,0,0 ' + __dirname + '/fixtures/plain_1.mbtiles ' + filepath, function(err, stdout, stderr) {
         t.ifError(err, 'no errors');
         t.ok(stdout.indexOf('100.0000%') !== -1, 'pct complete');
         t.ok(stdout.indexOf('86/    86') !== -1, '86/86');
@@ -43,7 +43,7 @@ test('copy bounds', function(t) {
 });
 
 test('copy list', function(t) {
-    exec(__dirname + '/../bin/copy --scheme=list --list=' + __dirname + '/fixtures/filescheme.flat ' + __dirname + '/fixtures/plain_1.mbtiles ' + filepath, function(err, stdout, stderr) {
+    exec(__dirname + '/../bin/tilelive-copy --scheme=list --list=' + __dirname + '/fixtures/filescheme.flat ' + __dirname + '/fixtures/plain_1.mbtiles ' + filepath, function(err, stdout, stderr) {
         t.ifError(err, 'no errors');
         t.ok(stdout.indexOf('100.0000%') !== -1, 'pct complete');
         t.ok(stdout.indexOf('77/    77') !== -1, '77/77');
